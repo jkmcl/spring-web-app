0ae42454cf9f65b43c7116906ce184b9191f7a2f
Michael Lee <jkmcl@users.noreply.github.com>
2023-11-12 23:53:46 +0800

    Upgrade Spring Boot

--- src/main/java/jkml/WebSecurityConfiguration.java
+++ src/main/java/jkml/WebSecurityConfiguration.java
@@ -1,16 +1,18 @@
 package jkml;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfiguration {
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().mvcMatchers("/authentication").authenticated();
-		http.authorizeRequests().anyRequest().permitAll();
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests().mvcMatchers("/authentication").authenticated();
+		http.authorizeHttpRequests().anyRequest().permitAll();
+		return http.build();
 	}
 
 }
